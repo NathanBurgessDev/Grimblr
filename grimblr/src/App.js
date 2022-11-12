1b67cf8addcd099efb97191f446cf28c3851b69f
Joseph Heyes <josephheyes@gmail.com>
2022-11-12 15:41:58 +0000

    make an alert thing it has broken the whole app its time to solve it

--- grimblr/src/App.js
+++ grimblr/src/App.js
@@ -1,15 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
+function handleInput(username) {
+  alert(username);
+}
+
+function FormSubmit() {
+  const [username, setUsername] = useState("");
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => { setUsername(e.target.value) }}
+      />
+      <button onClick={handleInput(username)}>Submit</button>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <input type="text" />
-        <button > Submit</button>
       </header>
+      <FormSubmit />
     </div>
+    
   );
 }
 
