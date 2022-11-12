2b2a2f7211b28c1d1e915dfdce789f00bfeaa926
Demi Oshin <demiaoshin@gmail.com>
2022-11-12 15:04:04 +0000

    add handle to App add onChange function to App add onClick button

--- grimblr/src/App.js
+++ grimblr/src/App.js
@@ -1,16 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+var handle = ""
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <input type="text" />
-        <button > Submit</button>
+        <input type="text" onChange={(e)=>setHandle(e.target.value)}/>
+        <button onclick={onSubmit}> Submit</button>
       </header>
     </div>
   );
 }
 
+function onSubmit() {
+  // send handle to api call
+}
+
+function setHandle(text) {
+  handle = text
+}
 export default App;
