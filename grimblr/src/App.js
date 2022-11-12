767a80f23ad7a16a87ed7c5956577bd12ec2fd41
Demi Oshin <demiaoshin@gmail.com>
2022-11-12 16:22:35 +0000

    change the logo file made the css better deleted old file

--- grimblr/src/App.js
+++ grimblr/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 import { useState } from 'react';
 
@@ -13,11 +13,13 @@ function FormSubmit () {
   return (
     <div>
       <input
+        class="inputs"
         type="text"
         value={username}
         onChange={(e) => { setUsername(e.target.value) }}
       />
-      <button onClick={() => handleInput(username)}>Submit</button>
+      <br></br>
+      <button class="buttons" onClick={() => handleInput(username)}>Submit</button>
     </div>
   );
 }
@@ -27,6 +29,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p> Enter your twitter handle below! </p>
         <FormSubmit />
       </header>
     </div>
