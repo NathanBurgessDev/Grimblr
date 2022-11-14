c7ecde778f89cf7dda7097b199af8be68056df6b
Joseph Heyes <josephheyes@gmail.com>
2022-11-14 14:32:21 +0000

    why am i committing this

--- grimblr/src/result.js
+++ grimblr/src/result.js
@@ -1,14 +1,21 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 import logo from './logo.png';
 
 function Result(data) {
-    const [name, setData] = useState("");
     data = data.json
+    const [name, setName] = useState("");
+    const changeName = () => setName(data);
+    
+    useEffect(() => {
+        changeName();
+    }, []);
+
     return (
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <p>hello: {data}</p>
+            <p>hello: {name}</p>
           </header>
         </div>
       );
